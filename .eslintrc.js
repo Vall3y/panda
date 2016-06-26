b06c22a196077d71122c2e33e70a9a3f38220322
Guy Zissman <vall3y@gmail.com>
2016-06-27 01:55:46 +0300

    Finished API

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     "extends": "airbnb",
     "plugins": [
-        "react"
-    ]
+        "react",
+    ],
+    "rules": {
+      "no-use-before-define": 0,
+      "no-underscore-dangle": 0,
+    }
 };
--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const dataRepo = require('./lib/data-repo');
+
+const upload = multer(); // for parsing multipart/form-data
 const app = express();
+
+// For parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('hello');
 });
 
 app.get('/comment', (req, res) => {
-  res.send('hello');
+  dataRepo.getComments()
+    .then(comments => {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(comments));
+    })
+    .catch(err => {
+      handleError(res, err);
+    });
 });
 
-app.post('/comment', (req, res) => {
-  res.send('hello');
+app.post('/comment', upload.array(), (req, res) => {
+  // Grab only relevant params to avoid saving unwanted data
+  const comment = {
+    email: req.body.email,
+    message: req.body.message,
+  };
+
+  dataRepo.insertComment(comment)
+    .then(id => {
+      res.send(id);
+    })
+    .catch(err => {
+      handleError(res, err);
+    });
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('App is up and running on port 3000');
 });
+
+function handleError(res, err) {
+  console.log('err:', err);
+  res.status(500);
+  res.send('There was a problem with the request');
+}
